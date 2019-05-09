5fee009ca31a4e1c71782e806a77cab12ffdb374
xxf098 <xxf098@gmail.com>
2019-05-08 22:23:05 -0400

    load config

--- mysql.py
+++ mysql.py
@@ -70,8 +70,7 @@ def print_show_table(result):
     create_table = result[0].get('Create Table', '')
     print(create_table)
 
-def run_sql_query(sql, conf):
-    config = conf.load()
+def run_sql_query(sql, config):
     connection = connect(**config, cursorclass=cursors.DictCursor)
     try:
         with connection.cursor() as cursor:
@@ -100,35 +99,40 @@ def get_all_tables(conf):
     finally:
         connection.close()
 
-#TODO: refactor
-def run_sql_query_new(sql):
+def load_config():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     config_path = os.path.join(dir_path, 'config.json')
     config = DBConfig.load(config_path)
+    return config
+
+def run_sql_query_new(sql, config=None):
+    config = load_config() if config is None else config
     connection = Connection(config)
     try:
             result = connection.run_sql(sql)
             if (re.match(r'^SHOW CREATE TABLE', sql, re.IGNORECASE)):
-                # [print(row[1]) for row in result.rows]
-                [print(x) for row in result.rows for x in row]
+                [print(row[1]) for row in result.rows]
     except Exception as e:
         print(traceback.print_exc())
     finally:
         connection.close()
 
-def get_all_tables_new():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    config_path = os.path.join(dir_path, 'config.json')
-    config = DBConfig.load(config_path)
+def get_all_tables_new(config=None):
+    config = load_config() if config is None else config
     connection = Connection(config)
     sql = "SELECT table_name FROM information_schema.tables WHERE table_type = 'base table' AND table_schema='{}'".format(config['db'])
     try:
             result = connection.run_sql(sql)
             [print(x[0]) for x in result.rows]
+            return result
     except Exception as e:
         print(traceback.print_exc())
     finally:
         connection.close()
+
+def synchronize_database_columns(data_path):
+    config = load_config()
+    tables = get_all_tables_new(config)
 
 def main():
     # parse args
@@ -138,15 +142,19 @@ def main():
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
     config_path = os.path.join(dir_path, 'config.json')
-    conf = Config(config_path)
+    config = Config(config_path).load()
 
-    arg = sys.argv[1]
-    if arg == '--table':
+    arg1 = sys.argv[1]
+    if arg1 == '--table':
         get_all_tables_new()
-    elif (re.match(r'^SHOW CREATE TABLE', arg, re.IGNORECASE)):
-        run_sql_query_new(arg)
+    elif arg1 == '--sync':
+        # data_path = os.path.join(dir_path, '.data', '{}_columns'.format(config['db']))
+        data_path = sys.argv[2] if len(sys.argv) > 2 else None
+        synchronize_database_columns(data_path)
+    elif (re.match(r'^SHOW CREATE TABLE', arg1, re.IGNORECASE)):
+        run_sql_query_new(arg1)
     else:
-        run_sql_query(arg, conf)
+        run_sql_query(arg1, config)
 
 #TODO: odbc flavor
 #TODO: vim refactor vscode mssql
