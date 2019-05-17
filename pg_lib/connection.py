c569758b0d085c803b02ec91731b50b03d37bd8a
xxf098 <xxf098@gmail.com>
2019-05-16 20:27:05 -0400

    init connection

--- pg_lib/connection.py
+++ pg_lib/connection.py
@@ -3,8 +3,17 @@ import socket
 class Connection():
 
     def __init__ (self,
-            user='postgres',
+            username='postgres',
             password='',
             host='localhoost',
-            port=5432):
-        pass
+            port=5432,
+            db=None,
+            connect_timeout=None):
+        self.host = host
+        self.username = username.encode('utf8') if isinstance(username, str) else username
+        self.password = password.encode('utf8') if isinstance(password, str) else password
+        self.db = db
+        self.port = port
+        self.connect_timeout = connect_timeout
+        self.encoding = 'utf8'
+
