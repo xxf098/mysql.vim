8f2b28db6ed37e26b6ea51dde05d9bbf1e2afe82
xxf098 <xxf098@gmail.com>
2019-05-16 20:14:14 -0400

    postgresql connector

---
+++ pg_lib/connection.py
@@ -0,0 +1,10 @@
+import socket
+
+class Connection():
+
+    def __init__ (self,
+            user='postgres',
+            password='',
+            host='localhoost',
+            port=5432):
+        pass
