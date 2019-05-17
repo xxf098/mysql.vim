0036af8ba55e30d2a0d247c5b84ef1887686b51f
xxf098 <xxf098@gmail.com>
2019-05-16 20:49:49 -0400

    connect to socket server

--- pg_lib/connection.py
+++ pg_lib/connection.py
@@ -16,4 +16,27 @@ class Connection():
         self.port = port
         self.connect_timeout = connect_timeout
         self.encoding = 'utf8'
+        self.connect()
 
+    def connect(self):
+        try:
+            self._usock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if self.connect_timeout is not None:
+                self._usock.settimeout(self.connect_timeout)
+            self._usock.connect((self.host, self.port))
+            self._sock = self._usock.makefile(mode="rwb")
+            self._usock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        except socket.error as e:
+            self._usock.close()
+            raise
+def main():
+    conn = Connection(
+            username="pguser",
+            host='localhost',
+            port=5432,
+            db="testdb",
+            password="123456",
+            connect_timeout=None)
+
+if __name__ == '__main__':
+    main()
