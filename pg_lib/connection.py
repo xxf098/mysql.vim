517002303b476e9012973c501a932971d38a0bd1
xxf098 <xxf098@gmail.com>
2019-05-17 20:35:31 -0400

    send startup message

--- pg_lib/connection.py
+++ pg_lib/connection.py
@@ -1,5 +1,5 @@
-import socket
-from pg_lib import CONST
+import socket, struct
+from pg_lib.CONST import NULL_BYTE
 
 class Connection():
 
@@ -16,26 +16,70 @@ class Connection():
         self.db = db
         self.port = port
         self.connect_timeout = connect_timeout
+        self._write_timeout = connect_timeout
+        self._read_timeout = connect_timeout
         self.encoding = 'utf8'
+        self._sock = None
         self.connect()
 
     def connect(self):
         try:
-            self._usock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             if self.connect_timeout is not None:
-                self._usock.settimeout(self.connect_timeout)
-            self._usock.connect((self.host, self.port))
-            self._sock = self._usock.makefile(mode="rwb")
-            self._usock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+                self._sock.settimeout(self.connect_timeout)
+            self._sock.connect((self.host, self.port))
+            self._rfile = self._sock.makefile(mode='rb')
+            self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
 
             self._startup_message()
         except socket.error as e:
-            self._usock.close()
+            self._sock.close()
             raise
 
     # https://www.postgresql.org/docs/9.1/protocol-message-formats.html
     def _startup_message(self):
         protocol = 196608
+        msg = struct.pack('!i', protocol) + b'user\x00' + self.username + NULL_BYTE
+        if self.db is not None:
+            database = self.db.encode('utf8') if isinstance(self.db, str) else self.db
+            msg = msg + b'database\x00' + database + NULL_BYTE
+        msg = msg + NULL_BYTE
+        self._write_bytes(struct.pack('!i', len(msg) + 4))
+        self._write_bytes(msg)
+        code, length = struct.unpack('!ci', self._read_bytes(5))
+        print(code)
+
+    def _write_bytes(self, bytes):
+        self._sock.settimeout(self._write_timeout)
+        try:
+            self._sock.sendall(bytes)
+        except IOError as e:
+            self._force_close()
+            raise
+
+    #TODO: refactor with mysql_lib
+    def _read_bytes(self, num_bytes):
+        self._sock.settimeout(self._read_timeout)
+        try:
+            data = self._rfile.read(num_bytes)
+        except:
+            self._force_close()
+            raise
+        if len(data) < num_bytes:
+            self._force_close()
+            raise Exception('fail to read data')
+        return data
+
+    def _force_close(self):
+        if self._sock is None:
+            return
+        try:
+            self._sock.close()
+        except:
+            pass
+        self._sock = None
+        self._rwfile = None
+
 
 def main():
     conn = Connection(
